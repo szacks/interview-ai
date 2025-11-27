30ed1006bffabce559de83603d51ab226d0f92e9
shaniZacks <shani.zacks@medaware.com>
2025-11-27 09:18:50 +0200

    first commit

---
+++ backend/src/test/java/com/example/interviewAI/InterviewAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
